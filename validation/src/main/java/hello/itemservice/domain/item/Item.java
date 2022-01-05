f1fb7f96e8eec3f8594ac18547ceaa799d843ee0
geneaky <kaktus418@gmail.com>
2022-01-05 15:07:13 +0900

    [ex] bean validation - validation object(form,request,dto)

--- validation/src/main/java/hello/itemservice/domain/item/Item.java
+++ validation/src/main/java/hello/itemservice/domain/item/Item.java
@@ -12,15 +12,15 @@ import org.hibernate.validator.constraints.ScriptAssert;
 //기능적으로 사용하기 불편함 그냥 자바 코드로 validation하는게 더 편한듯
 public class Item {
 
-    @NotNull(groups = UpdateCheck.class)
+//    @NotNull(groups = UpdateCheck.class)
     private Long id;
-    @NotBlank(groups = {SaveCheck.class,UpdateCheck.class})
+//    @NotBlank(groups = {SaveCheck.class,UpdateCheck.class})
     private String itemName;
-    @NotNull(groups = {SaveCheck.class,UpdateCheck.class})
-    @Range(min = 1000, max = 1000000, groups = {SaveCheck.class,UpdateCheck.class})
+//    @NotNull(groups = {SaveCheck.class,UpdateCheck.class})
+//    @Range(min = 1000, max = 1000000, groups = {SaveCheck.class,UpdateCheck.class})
     private Integer price;
-    @NotNull(groups = {SaveCheck.class,UpdateCheck.class})
-    @Max(value = 9999,groups = SaveCheck.class)
+//    @NotNull(groups = {SaveCheck.class,UpdateCheck.class})
+//    @Max(value = 9999,groups = SaveCheck.class)
     private Integer quantity;
 
     public Item() {

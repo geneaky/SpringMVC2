632cebf43f2d0a1da741915442594567800fae9b
geneaky <kaktus418@gmail.com>
2022-01-06 21:59:05 +0900

    [ex] servlet error handle constant

--- exception/src/main/java/hello/exception/servlet/ErrorPageController.java
+++ exception/src/main/java/hello/exception/servlet/ErrorPageController.java
@@ -1,5 +1,10 @@
 package hello.exception.servlet;
 
+import static javax.servlet.RequestDispatcher.ERROR_MESSAGE;
+import static javax.servlet.RequestDispatcher.ERROR_REQUEST_URI;
+import static javax.servlet.RequestDispatcher.ERROR_SERVLET_NAME;
+import static javax.servlet.RequestDispatcher.ERROR_STATUS_CODE;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
@@ -19,6 +24,14 @@ public class ErrorPageController {
   @RequestMapping("/error-page/500")
   public String errorPage500(HttpServletRequest request, HttpServletResponse response) {
     log.info("errorPage 500");
+    printErrorInfo(request);
     return "error-page/500";
+  }
+
+  private void printErrorInfo(HttpServletRequest request) {
+    log.info(String.valueOf(request.getAttribute(ERROR_MESSAGE)));
+    log.info(String.valueOf(request.getAttribute(ERROR_REQUEST_URI)));
+    log.info(String.valueOf(request.getAttribute(ERROR_SERVLET_NAME)));
+    log.info(String.valueOf(request.getAttribute(ERROR_STATUS_CODE)));
   }
 }
